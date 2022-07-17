46ab72004298741d020f183c70da7ccdb991ccc9
Kays-L <1kayslaouar@gmail.com>
2022-07-17 15:36:15 -0400

    first commit

---
+++ uf.js
@@ -0,0 +1,15 @@
+class UnionFind {
+    constructor(arr) {
+        const n = arr.length;
+        this.sets = arr.slice();
+        this.sizes = new Array(n).fill(1);
+    }
+
+    union(x, y) {
+        
+    }
+
+    find(x) {
+
+    }
+}
